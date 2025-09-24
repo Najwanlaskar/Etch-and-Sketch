f7fe88597ae1af3fc02b8cd3e76b7f620e4d16f3
Najwanlaskar <Najwanlaskar12@example.com>
2025-09-24 18:23:54 +0100

    Added index.html, script.js and styles.css
    Added structure to index.html and linked them to script.js and styles.css

---
+++ script.js
@@ -0,0 +1,8 @@
+//put the gridn squares inside a container div (HTML)
+//16x16 square grid of div
+//FlexBox can be used to make the divs appear as a grid
+//set a hovering effect so grid divs change color when 
+//you hover over them
+//add a button top of the screen that will promt the user for the number of squares per side wanted
+//limit should be 100
+//new grid should be same size as the other ones
